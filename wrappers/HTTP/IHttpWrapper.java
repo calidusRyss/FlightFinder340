e334b380851e4735848fc1012dd13b8206f0a5c8
Teegan Krieger <68746768+TeeganKrieger@users.noreply.github.com>
2020-09-30 15:33:03 -0400

    HTTP wrapper interface throws proper exceptions
    The HTTP wrapper interface now throws an InvalidUrlException and a ConnectionFailedException.

--- wrappers/HTTP/IHttpWrapper.java
+++ wrappers/HTTP/IHttpWrapper.java
@@ -1,5 +1,7 @@
 package main.java.wrappers.HTTP;
 
+import main.java.exceptions.HTTP.ConnectionFailedException;
+import main.java.exceptions.HTTP.InvalidUrlException;
 import main.java.models.HTTP.Request;
 import main.java.models.HTTP.Response;
 
@@ -11,6 +13,6 @@ import main.java.models.HTTP.Response;
  */
 public interface IHttpWrapper {
 
-    public Response callRequest(Request _request);
+    public Response callRequest(Request _request) throws InvalidUrlException, ConnectionFailedException;
 
 }
