af41ec02772e2dfb7304bab185b8f591a7beeaaa
calidusRyss <69991651+calidusRyss@users.noreply.github.com>
2020-10-28 14:47:12 -0400

    michel struct files

---
+++ models/flightapi/structures/property.java
@@ -0,0 +1,36 @@
+package flightfinder3.main.java.FlightFinder340.models.flightapi.structures;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Callidus
+ */
+public class property {
+    public final String name;
+    public final String content;
+    
+    public property(String n, String c)
+    {
+        name = n.toLowerCase();
+        content = c;
+    }
+    
+    public static property[] createProperty(String[] s)
+    {
+        property[] output = new property[s.length/2];
+        
+        for (int i = 0; i < s.length; i+=2)
+        {
+            property p = new property(s[i],s[i+1]);
+            
+            output[i/2] = p;
+        }
+        
+        return output;        
+    }
+}
